02af49fd29b943ceefd99d1e22603299ff214f4c
haikirei <cjin1@pride.hofstra.edu>
2022-01-06 14:32:21 -0500

    connecting api

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+    # pk_ebfa12885d9f421fbc30a04d37231325
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_ebfa12885d9f421fbc30a04d37231325")
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+    return render(request, 'home.html', {'api':api})
 
 def about(request):
     return render(request, 'about.html', {})
